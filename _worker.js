e68326b30af14ad222ecfe88c5008a36f9aeb893
wytedu <waiyantun.edu@gmail.com>
2025-07-12 14:16:38 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd70ff7eb-2484-42b4-8f48-210951b43dd0';
 
-const พร็อกซีไอพีs = ['172.67.200.228', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
+const พร็อกซีไอพีs = ['workers.cloudflare.cyou'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
